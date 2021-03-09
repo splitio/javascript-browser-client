5967f5fbcd5d09f52220a8d64ff17bf8a00ca04e
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-03-09 13:41:28 -0300

    removed comments

--- src/umd.ts
+++ src/umd.ts
@@ -3,19 +3,11 @@ import { SplitFactory } from './splitFactory';
 import { InLocalStorage } from '@splitsoftware/splitio-commons/src/storages/inLocalStorage/index';
 import { default as GoogleAnalyticsToSplit } from '@splitsoftware/splitio-commons/src/integrations/ga/GaToSplitPlugin';
 import { default as SplitToGoogleAnalytics } from '@splitsoftware/splitio-commons/src/integrations/ga/SplitToGaPlugin';
-// import { errorLogger } from '@splitsoftware/splitio-commons/src/logger/errorLogger';
-// import { warnLogger } from '@splitsoftware/splitio-commons/src/logger/warnLogger';
-// import { infoLogger } from '@splitsoftware/splitio-commons/src/logger/infoLogger';
-// import { debugLogger } from '@splitsoftware/splitio-commons/src/logger/debugLogger';
 
 // Include all pluggable modules as properties to expose at the global `splitio` object
 SplitFactory.SplitFactory = SplitFactory;
 SplitFactory.InLocalStorage = InLocalStorage;
 SplitFactory.GoogleAnalyticsToSplit = GoogleAnalyticsToSplit;
 SplitFactory.SplitToGoogleAnalytics = SplitToGoogleAnalytics;
-// SplitFactory.errorLogger = errorLogger;
-// SplitFactory.warnLogger = warnLogger;
-// SplitFactory.infoLogger = infoLogger;
-// SplitFactory.debugLogger = debugLogger;
 
 export default SplitFactory;
