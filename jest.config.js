2f8aa00a1c98e1a56935c920e4fca82ed00668f3
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-03-25 14:04:33 -0300

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,10 @@ module.exports = {
   testMatch: ['<rootDir>/src/**/__tests__/**/?(*.)+(spec|test).[jt]s'],
 
   // Exclude E2E tests
-  testPathIgnorePatterns: ['<rootDir>/src/__tests__']
+  testPathIgnorePatterns: ['<rootDir>/src/__tests__'],
+
+  // Required when using Split dependencies via GitHub
+  // `"@splitsoftware/splitio-commons": "github:splitio/javascript-commons#branch_name",`
+  transform: { 'node_modules/@splitsoftware/.+\\.(j|t)s$': 'ts-jest' },
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!@splitsoftware/.*)']
 };
