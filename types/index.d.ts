5c11e04d49fbf570fe744ff92ff25b715c5d4406
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-11-05 18:12:59 -0300

    updated type definitions

--- types/index.d.ts
+++ types/index.d.ts
@@ -16,6 +16,7 @@ declare module JsSdk {
    * For more information read the corresponding article: @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#configuration}
    */
   export function SplitFactory(settings: SplitIO.IBrowserSettings): SplitIO.ISDK;
+  export function SplitFactory(settings: SplitIO.IBrowserSettingsAsync): SplitIO.IAsyncSDK;
 
   /**
    * Persistent storage based on the LocalStorage Web API for browsers.
@@ -23,6 +24,13 @@ declare module JsSdk {
    * @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#storage}
    */
   export function InLocalStorage(options?: SplitIO.InLocalStorageOptions): SplitIO.StorageSyncFactory;
+
+  /**
+   * Pluggable storage to use the SDK in consumer mode.
+   *
+   * @see {@link @TODO}
+   */
+  export function PluggableStorage(options?: SplitIO.PluggableStorageOptions): SplitIO.StorageAsyncFactory;
 
   /**
    * Enable 'Google Analytics to Split' integration, to track Google Analytics hits as Split events.
