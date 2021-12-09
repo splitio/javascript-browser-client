5a8598a2614d901f9a29b6e74769a6734a98c0ab
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-12-09 20:22:58 -0300

    new rc

--- src/settings/defaults.ts
+++ src/settings/defaults.ts
@@ -1,7 +1,7 @@
 import { LogLevels, isLogLevelString } from '@splitsoftware/splitio-commons/src/logger/index';
 import { LogLevel } from '@splitsoftware/splitio-commons/src/types';
 
-const packageVersion = '0.2.1-rc.0';
+const packageVersion = '0.2.1-rc.1';
 
 /**
  * In browser, the default debug level, can be set via the `localStorage.splitio_debug` item.
