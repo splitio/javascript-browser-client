e155d8ea663598f19a1109d92019ebf51560c938
Emiliano Sanchez <emiliano.sanchez@split.io>
2022-03-08 18:14:57 -0300

    upgrade js-commons

--- src/platform/getModules.ts
+++ src/platform/getModules.ts
@@ -10,7 +10,7 @@ import { pluggableIntegrationsManagerFactory } from '@splitsoftware/splitio-comm
 
 import { shouldAddPt } from '@splitsoftware/splitio-commons/src/trackers/impressionObserver/utils';
 import { IPlatform, ISdkFactoryParams } from '@splitsoftware/splitio-commons/src/sdkFactory/types';
-import { SplitIO, ISettings } from '@splitsoftware/splitio-commons/src/types';
+import { ISettings } from '@splitsoftware/splitio-commons/src/types';
 import { CONSUMER_MODE, CONSUMER_PARTIAL_MODE, LOCALHOST_MODE } from '@splitsoftware/splitio-commons/src/utils/constants';
 import { userConsentProps } from '@splitsoftware/splitio-commons/src/sdkFactory/userConsentProps';
 
@@ -37,8 +37,6 @@ export function getModules(settings: ISettings, platform: IPlatform): ISdkFactor
     sdkClientMethodFactory: sdkClientMethodCSFactory,
 
     SignalListener: BrowserSignalListener as ISdkFactoryParams['SignalListener'],
-
-    impressionListener: settings.impressionListener as SplitIO.IImpressionListener,
 
     integrationsManagerFactory: settings.integrations && settings.integrations.length > 0 ? pluggableIntegrationsManagerFactory.bind(null, settings.integrations) : undefined,
 
