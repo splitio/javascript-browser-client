82c20f2cdfb65fe71880ea9d65b103d52c7f4797
Emiliano Sanchez <emiliano.sanchez@split.io>
2022-09-19 19:46:45 -0300

    updated e2e tests for partial consumer mode with pluggable storage to validate NONE impressions mode

--- src/__tests__/consumer/browser_consumer_partial.spec.js
+++ src/__tests__/consumer/browser_consumer_partial.spec.js
@@ -194,4 +194,151 @@ tape('Browser Consumer Partial mode with pluggable storage', function (t) {
     });
   });
 
+  t.test('Regular usage - NONE strategy', async (assert) => {
+
+    fetchMock.postOnce(url(config, '/testImpressions/count'), 200);
+    fetchMock.postOnce(url(config, '/v1/keys/cs'), (url, req) => {
+      const data = JSON.parse(req.body);
+
+      assert.deepEqual(data, {
+        keys: [
+          {
+            k: 'UT_Segment_member',
+            fs: ['UT_IN_SEGMENT', 'UT_NOT_IN_SEGMENT', 'UT_SET_MATCHER', 'UT_NOT_SET_MATCHER', 'always-o.n-with-config', 'always-on', 'hierarchical_splits_testing_on', 'hierarchical_splits_testing_off', 'hierarchical_splits_testing_on_negated']
+          },
+          {
+            k: 'emi@split.io',
+            fs: ['UT_IN_SEGMENT', 'UT_NOT_IN_SEGMENT']
+          },
+          {
+            k: 'other',
+            fs: ['UT_IN_SEGMENT']
+          }]
+      }, 'We performed evaluations for 3 keys, so we should have 3 items total.');
+
+      return 200;
+    });
+
+    fetchMock.postOnce(url(config, '/events/bulk'), (url, req) => {
+      const resp = JSON.parse(req.body);
+      assert.equal(resp.length, TOTAL_EVENTS, 'All successfully tracked events were sent');
+      return 200;
+    });
+
+    // Load wrapper with data to do the proper tests
+    const wrapperInstance = inMemoryWrapperFactory();
+    await applyOperations(wrapperInstance);
+
+    /** @type SplitIO.ImpressionData[] */
+    const impressions = [];
+    const disconnectSpy = sinon.spy(wrapperInstance, 'disconnect');
+
+    // Overwrite Math.random to NOT track telemetry
+    const originalMathRandom = Math.random; Math.random = () => 0.5;
+    const sdk = SplitFactory({
+      ...config,
+      storage: PluggableStorage({
+        prefix: wrapperPrefix,
+        wrapper: wrapperInstance
+      }),
+      sync: {
+        impressionsMode: 'NONE'
+      },
+      impressionListener: {
+        logImpression(data) { impressions.push(data); }
+      }
+    });
+    Math.random = originalMathRandom; // restore
+
+    const client = sdk.client();
+    const otherClient = sdk.client('emi@split.io');
+
+    /** Evaluation, track and manager methods before SDK_READY */
+
+    const getTreatmentResult = client.getTreatment('UT_IN_SEGMENT');
+
+    assert.equal(typeof getTreatmentResult.then, 'function', 'GetTreatment calls should always return a promise on Consumer mode.');
+    assert.equal(await getTreatmentResult, 'control', 'Evaluations using pluggable storage should be control if initiated before SDK_READY.');
+    assert.equal(client.__getStatus().isReadyFromCache, false, 'SDK in consumer mode is not operational inmediatelly');
+    assert.equal(client.__getStatus().isReady, false, 'SDK in consumer mode is not operational inmediatelly');
+
+    const trackResult = otherClient.track('user', 'test.event', 18);
+    assert.equal(typeof trackResult.then, 'function', 'Track calls should always return a promise on Consumer mode.');
+    assert.true(await trackResult, 'If the wrapper operation success to queue the event, the promise will resolve to true');
+
+    /** Evaluation, track and manager methods on SDK_READY */
+
+    await client.ready();
+    await otherClient.ready(); // waiting to avoid 'control' with label 'sdk not ready'
+
+    assert.equal(await client.getTreatment('UT_IN_SEGMENT'), 'on', '`getTreatment` evaluation using pluggable storage should be correct.');
+    assert.equal((await otherClient.getTreatmentWithConfig('UT_IN_SEGMENT')).treatment, 'off', '`getTreatmentWithConfig` evaluation using pluggable storage should be correct.');
+
+    assert.equal((await client.getTreatments(['UT_NOT_IN_SEGMENT']))['UT_NOT_IN_SEGMENT'], 'off', '`getTreatments` evaluation using pluggable storage should be correct.');
+    assert.equal((await otherClient.getTreatmentsWithConfig(['UT_NOT_IN_SEGMENT']))['UT_NOT_IN_SEGMENT'].treatment, 'on', '`getTreatmentsWithConfig` evaluation using pluggable storage should be correct.');
+
+    assert.equal(await client.getTreatment('UT_SET_MATCHER', {
+      permissions: ['admin']
+    }), 'on', 'Evaluations using pluggable storage should be correct.');
+    assert.equal(await client.getTreatment('UT_SET_MATCHER', {
+      permissions: ['not_matching']
+    }), 'off', 'Evaluations using pluggable storage should be correct.');
+
+    assert.equal(await client.getTreatment('UT_NOT_SET_MATCHER', {
+      permissions: ['create']
+    }), 'off', 'Evaluations using pluggable storage should be correct.');
+    assert.equal(await client.getTreatment('UT_NOT_SET_MATCHER', {
+      permissions: ['not_matching']
+    }), 'on', 'Evaluations using pluggable storage should be correct.');
+    assert.deepEqual(await client.getTreatmentWithConfig('UT_NOT_SET_MATCHER', {
+      permissions: ['not_matching']
+    }), {
+      treatment: 'on',
+      config: null
+    }, 'Evaluations using pluggable storage should be correct, including configs.');
+    assert.deepEqual(await client.getTreatmentWithConfig('always-o.n-with-config'), {
+      treatment: 'o.n',
+      config: '{"color":"brown"}'
+    }, 'Evaluations using pluggable storage should be correct, including configs.');
+
+    assert.equal(await client.getTreatment('always-on'), 'on', 'Evaluations using pluggable storage should be correct.');
+
+    // Below splits were added manually.
+    // They are all_keys (always evaluate to on) which depend from always-on split. the _on/off is what treatment they are expecting there.
+    assert.equal(await client.getTreatment('hierarchical_splits_testing_on'), 'on', 'Evaluations using pluggable storage should be correct.');
+    assert.equal(await client.getTreatment('hierarchical_splits_testing_off'), 'off', 'Evaluations using pluggable storage should be correct.');
+    assert.equal(await client.getTreatment('hierarchical_splits_testing_on_negated'), 'off', 'Evaluations using pluggable storage should be correct.');
+
+    assert.equal(typeof client.track('user', 'test.event', 18).then, 'function', 'Track calls should always return a promise on Consumer mode.');
+    assert.equal(typeof client.track().then, 'function', 'Track calls should always return a promise on Consumer mode, even when parameters are incorrect.');
+
+    assert.true(await client.track('user', 'test.event', 18), 'If the event was succesfully queued the promise will resolve to true');
+    assert.false(await client.track(), 'If the event was NOT succesfully queued the promise will resolve to false');
+
+    // New shared client created
+    const newClient = sdk.client('other');
+    newClient.track('user', 'test.event', 18).then(result => assert.true(result, 'Track should attempt to track, whether SDK_READY has been emitted or not.'));
+    newClient.getTreatment('UT_IN_SEGMENT').then(result => assert.equal(result, 'control', '`getTreatment` evaluation is control if shared client is not ready yet.'));
+
+    await newClient.ready();
+    assert.true(await newClient.track('user', 'test.event', 18), 'Track should attempt to track, whether SDK_READY has been emitted or not.');
+    assert.equal((await newClient.getTreatment('UT_IN_SEGMENT')), 'off', '`getTreatment` evaluation using pluggable storage should be correct.');
+
+    await client.ready(); // promise already resolved
+    await newClient.destroy();
+    await otherClient.destroy();
+
+    await client.destroy();
+
+    assert.equal(disconnectSpy.callCount, 1, 'Wrapper disconnect method should be called only once, when the main client is destroyed');
+
+    // Assert impressionsListener
+    setTimeout(() => {
+      assert.equal(impressions.length, TOTAL_RAW_IMPRESSIONS, 'Each evaluation has its corresponting impression');
+      assert.equal(impressions[0].impression.label, SDK_NOT_READY, 'The first impression is control with label "sdk not ready"');
+
+      assert.end();
+    });
+  });
+
 });
