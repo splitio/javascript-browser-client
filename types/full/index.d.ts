51f86a24362cd55a73848f45f08da1b592628874
Emiliano Sanchez <emiliano.sanchez@split.io>
2025-09-18 14:10:19 -0300

    stable version

--- types/full/index.d.ts
+++ types/full/index.d.ts
@@ -10,10 +10,10 @@ declare module JsSdk {
   /**
    * Full version of the Split.io SDK factory function.
    *
-   * Unlike the default version, it includes a `fetch` polyfill to support old browsers @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#language-support}.
+   * Unlike the default version, it includes a `fetch` polyfill to support old browsers @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/browser-sdk/#language-support}.
    *
    * The settings parameter should be an object that complies with the SplitIO.IClientSideSettings or SplitIO.IClientSideAsyncSettings interfaces.
-   * For more information read the corresponding article: @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#configuration}
+   * For more information read the corresponding article: @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/browser-sdk/#configuration}
    */
   export function SplitFactory(settings: SplitIO.IClientSideSettings): SplitIO.IBrowserSDK;
   export function SplitFactory(settings: SplitIO.IClientSideAsyncSettings): SplitIO.IBrowserAsyncSDK;
@@ -21,42 +21,42 @@ declare module JsSdk {
   /**
    * Persistent storage based on the LocalStorage Web API for browsers.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#storage}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/browser-sdk/#storage}
    */
   export function InLocalStorage(options?: SplitIO.InLocalStorageOptions): SplitIO.StorageSyncFactory;
 
   /**
    * Pluggable storage to use the SDK in consumer mode.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#sharing-state-with-a-pluggable-storage}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/browser-sdk/#sharing-state-with-a-pluggable-storage}
    */
   export function PluggableStorage(options: SplitIO.PluggableStorageOptions): SplitIO.StorageAsyncFactory;
 
   /**
    * Creates a logger instance that enables descriptive log messages with DEBUG log level when passed in the factory settings.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#logging}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/browser-sdk/#logging}
    */
   export function DebugLogger(): SplitIO.ILogger;
 
   /**
    * Creates a logger instance that enables descriptive log messages with INFO log level when passed in the factory settings.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#logging}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/browser-sdk/#logging}
    */
   export function InfoLogger(): SplitIO.ILogger;
 
   /**
    * Creates a logger instance that enables descriptive log messages with WARN log level when passed in the factory settings.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#logging}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/browser-sdk/#logging}
    */
   export function WarnLogger(): SplitIO.ILogger;
 
   /**
    * Creates a logger instance that enables descriptive log messages with ERROR log level when passed in the factory settings.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#logging}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/browser-sdk/#logging}
    */
   export function ErrorLogger(): SplitIO.ILogger;
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -11,50 +11,50 @@ declare module JsSdk {
    * Split.io SDK factory function.
    *
    * The settings parameter should be an object that complies with the SplitIO.IClientSideSettings or SplitIO.IClientSideAsyncSettings interfaces.
-   * For more information read the corresponding article: @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#configuration}
+   * For more information read the corresponding article: @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/browser-sdk/#configuration}
    */
   export function SplitFactory(settings: SplitIO.IClientSideSettings): SplitIO.IBrowserSDK;
   export function SplitFactory(settings: SplitIO.IClientSideAsyncSettings): SplitIO.IBrowserAsyncSDK;
 
   /**
-   * Persistent storage. By default, it uses the browser's LocalStorage API.
+   * Persistent storage. By default, it uses the browser's LocalStorage API if available.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#configuring-localstorage-cache-for-the-sdk}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/browser-sdk/#configuring-persistent-cache-for-the-sdk}
    */
   export function InLocalStorage(options?: SplitIO.InLocalStorageOptions): SplitIO.StorageSyncFactory;
 
   /**
    * Pluggable storage to use the SDK in consumer mode.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#sharing-state-with-a-pluggable-storage}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/browser-sdk/#sharing-state-with-a-pluggable-storage}
    */
   export function PluggableStorage(options: SplitIO.PluggableStorageOptions): SplitIO.StorageAsyncFactory;
 
   /**
    * Creates a logger instance that enables descriptive log messages with DEBUG log level when passed in the factory settings.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#logging}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/browser-sdk/#logging}
    */
   export function DebugLogger(): SplitIO.ILogger;
 
   /**
    * Creates a logger instance that enables descriptive log messages with INFO log level when passed in the factory settings.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#logging}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/browser-sdk/#logging}
    */
   export function InfoLogger(): SplitIO.ILogger;
 
   /**
    * Creates a logger instance that enables descriptive log messages with WARN log level when passed in the factory settings.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#logging}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/browser-sdk/#logging}
    */
   export function WarnLogger(): SplitIO.ILogger;
 
   /**
    * Creates a logger instance that enables descriptive log messages with ERROR log level when passed in the factory settings.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/360058730852-Browser-SDK#logging}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/browser-sdk/#logging}
    */
   export function ErrorLogger(): SplitIO.ILogger;
 }
